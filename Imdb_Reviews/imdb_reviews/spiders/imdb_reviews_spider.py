c2a9e7b4ce5c68dc58d9af421c662edbafa874f6
IsaAfonsoCollares <isabela_afonso_collares@hotmail.com>
2023-03-26 08:03:49 -0300

    reviews

--- Imdb_Reviews/imdb_reviews/spiders/imdb_reviews_spider.py
+++ Imdb_Reviews/imdb_reviews/spiders/imdb_reviews_spider.py
@@ -20,11 +20,11 @@ class ImdbReviewsSpider(scrapy.Spider):
     def parse(self, response):
         for review in response.css(".review-container"):
             yield {
-                "title": review.css(".title::text").get(),
+                "title": review.css(".title::text").get().strip(),
                 "author": review.css(".display-name-link a::text").get(),
                 "date": review.css(".review-date::text").get(),
                 "rating": review.css(".rating-other-user-rating span::text").get(),
-                "text": review.css(".text::text").get()
+                "text": review.css(".text::text").get().strip()
             }
 
         next_page = response.css(".load-more-data").attrib["data-key"]
